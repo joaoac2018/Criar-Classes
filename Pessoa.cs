e8e8bcfd1e6d36c9dc787c3a90957e172ab4b932
João Paulo Cruz <43938376+joaoac2018@users.noreply.github.com>
2020-09-01 11:56:12 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriarClasse
+{
+    class Pessoa
+    {
+        public String Nome { get => Nome; set => Nome = value; }
+        public String Endereco { get => Endereco; set => Endereco = value; }
+        public String Telefone { get => Telefone; set => Telefone = value; }
+        
+        public Pessoa() { }
+
+        public Pessoa(String Nome, String Endereco, String Telefone)
+        {
+            this.Nome = Nome;
+            this.Endereco = Endereco;
+            this.Telefone = Telefone;
+        }
+
+        public void setNome(String Nome)
+        {
+            this.Nome = Nome;
+        }
+
+        public String getNome()
+        {
+            return this.Nome;
+        }
+
+        public void setEndereco(String Endereco)
+        {
+            this.Endereco = Endereco;
+        }
+
+        public String getEndereco()
+        {
+            return this.Endereco;
+        }
+
+        public void setTelefone(String Telefone)
+        {
+            this.Telefone = Telefone;
+        }
+
+        public String getTelefone()
+        {
+            return this.Telefone;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriarClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.setNome("Renato Violin");
+            p1.setEndereco("Rua: Alessandro silva, 20");
+            p1.setTelefone("1140132432");
+        }
+    }
+}
